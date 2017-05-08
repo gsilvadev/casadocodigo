6d0dfdec0efd3e13e17e425d5ca10347ee898309
Gilson Oliveira <gsilva.dev@gmail.com>
2017-05-08 07:06:04 -0300

    Implementação até a aula 3 do curso 2 de Spring
    Habilitando cache, adicionando negociação de conteúdo e adicionando bootstrap.

--- src/main/java/br/com/casadocodigo/loja/conf/WebAppConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/WebAppConfiguration.java
@@ -1,5 +1,13 @@
 package br.com.casadocodigo.loja.conf;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,13 +16,19 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.google.common.cache.CacheBuilder;
 
 import br.com.casadocodigo.loja.controllers.HomeController;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
@@ -22,55 +36,83 @@ import br.com.casadocodigo.loja.infra.FileSaver;
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 
 @EnableWebMvc
-@ComponentScan(basePackageClasses={HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class})
-public class WebAppConfiguration extends WebMvcConfigurerAdapter{
+@ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class })
+@EnableCaching
+public class WebAppConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
-	public InternalResourceViewResolver internalResourceViewResolver(){
+	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
-		
+
 		resolver.setExposedContextBeanNames("carrinhoCompras");
-		
+
 		return resolver;
 	}
-	
+
 	@Bean
-	public MessageSource messageSource(){
+	public MessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		
+
 		messageSource.setBasename("/WEB-INF/messages");
 		messageSource.setDefaultEncoding("UTF-8");
 		messageSource.setCacheSeconds(1);
-		
-		return messageSource ;
+
+		return messageSource;
 	}
-	
+
 	@Bean
-	public FormattingConversionService mvcConversionService(){
+	public FormattingConversionService mvcConversionService() {
 		DefaultFormattingConversionService conversionServide = new DefaultFormattingConversionService();
 		DateFormatterRegistrar registrar = new DateFormatterRegistrar();
 		registrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
-		
+
 		registrar.registerFormatters(conversionServide);
-		
+
 		return conversionServide;
 	}
-	
+
 	@Bean
-	public MultipartResolver multipartResolver(){
+	public MultipartResolver multipartResolver() {
 		return new StandardServletMultipartResolver();
 	}
+
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//		super.addResourceHandlers(registry);
+//	}
 	
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-		super.addResourceHandlers(registry);
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	    configurer.enable();
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder().maximumSize(100)
+				.expireAfterAccess(5, TimeUnit.MINUTES);
+		GuavaCacheManager manager = new GuavaCacheManager();
+		manager.setCacheBuilder(builder);
+		return new ConcurrentMapCacheManager();
 	}
 	
 	@Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
+	public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager){
+		List<ViewResolver> viewResolvers = new ArrayList<>();
+		viewResolvers.add(this.internalResourceViewResolver());
+		viewResolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(viewResolvers);
+		resolver.setContentNegotiationManager(manager);
+		
+		return resolver;
+	}
 }
--- src/main/webapp/WEB-INF/views/produtos/lista.jsp
+++ src/main/webapp/WEB-INF/views/produtos/lista.jsp
@@ -1,36 +1,71 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!--     import tag lib -->
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Livros de Java, Android, iPhone, PHP, Ruby e Muito mais - Casa Do Código</title>
+<title>Livros de Java, Android, iPhone, PHP, Ruby e Muito mais -
+	Casa Do Código</title>
+<c:url value="/resources/css" var="cssPath" />
+<link rel="stylesheet" href="${cssPath }/bootstrap.min.css">
+
+<link rel="stylesheet" href="${cssPath }/bootstrap-theme.min.css">
+<style type="text/css">
+body {
+	padding: 0px 0px 60px 0px;
+}
+</style>
 </head>
 <body>
-	<h1>Lista de Produtos</h1>
-	<div>${sucesso }</div>
-	<div>${falha }</div>
-	<table>
-		<thead>
-			<tr>
-				<th>Título</th>
-				<th>Descrição</th>
-				<th>Páginas</th>
-			</tr>
-		</thead>
-		<tbody>
-			<c:forEach items="${produtos }" var="produto">
+	<nav class="navbar navbar-inverse">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed"
+				data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+				aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="${s:mvcUrl('HC#index').build() }">Casa do Código</a>
+		</div>
+		<div class="collapse navbar-collapse"
+			id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li><a href="${s:mvcUrl('PC#listar').build() }">Lista de Produtos</a></li>
+				<li><a href="${s:mvcUrl('PC#form').build() }">Cadastro de Produtos</a></li>
+			</ul>
+		</div>
+		<!-- /.navbar-collapse -->
+	</div>
+	</nav>
+	<div class="container">
+		<h1>Lista de Produtos</h1>
+		<div>${sucesso }</div>
+		<div>${falha }</div>
+		<table class="table table-bordered table-striped table-hover">
+			<thead>
 				<tr>
-					<td><a href="${s:mvcUrl('PC#detalhe').arg(0, produto.id).build() }">${produto.titulo }</a></td>
-					<td>${produto.descricao }</td>
-					<td>${produto.paginas }</td>
+					<th>Título</th>
+					<th>Descrição</th>
+					<th>Páginas</th>
 				</tr>
-			</c:forEach>
-		</tbody>
-	</table>
+			</thead>
+			<tbody>
+				<c:forEach items="${produtos }" var="produto">
+					<tr>
+						<td><a
+							href="${s:mvcUrl('PC#detalhe').arg(0, produto.id).build() }">${produto.titulo }</a></td>
+						<td>${produto.descricao }</td>
+						<td>${produto.paginas }</td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</div>
 </body>
 </html>
