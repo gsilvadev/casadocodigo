574bc8b9e6a5144de38bf2491fc3939796b6f1f5
Gilson Oliveira <gsilva.dev@gmail.com>
2017-05-28 15:20:35 -0300

    Adicionando envio de email ao finalizar compra

--- src/main/java/br/com/casadocodigo/loja/conf/WebAppConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/WebAppConfiguration.java
@@ -2,6 +2,7 @@ package br.com.casadocodigo.loja.conf;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
@@ -16,6 +17,8 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
@@ -127,5 +130,23 @@ public class WebAppConfiguration extends WebMvcConfigurerAdapter {
 	@Bean
 	public LocaleResolver localeResolver(){
 		return new CookieLocaleResolver();
+	}
+	
+	@Bean
+	public MailSender mailSender(){
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+		//TODO Modivicar Informações para o servidor de email
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setUsername("email@gmail.com");
+		mailSender.setPassword("123456");
+		mailSender.setPort(587);
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", true);
+		props.put("mail.smtp.starttls.enable", true);
+		mailSender.setJavaMailProperties(props);
+		
+		return mailSender;
 	}
 }
--- src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -4,10 +4,12 @@ import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 import br.com.casadocodigo.loja.models.DadosPagamento;
+import br.com.casadocodigo.loja.models.Usuario;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -27,10 +30,14 @@ public class PagamentoController {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Autowired
+	private MailSender sender;
+
 	@RequestMapping(value="finalizar", method=RequestMethod.POST)
-	public Callable<ModelAndView> finalizar(RedirectAttributes model) {
+	public Callable<ModelAndView> finalizar(@AuthenticationPrincipal Usuario usuario, RedirectAttributes model) {
 		return () -> {
 			try {
+				enviaEmail(usuario);
 				String url = "http://book-payment.herokuapp.com/payment";
 				String response = restTemplate.postForObject(url, new DadosPagamento(carrinho.getTotal()), String.class);
 				model.addFlashAttribute("sucesso", response);
@@ -42,5 +49,16 @@ public class PagamentoController {
 				return new ModelAndView("redirect:/produtos");
 			}
 		};
+	}
+
+	private void enviaEmail(Usuario usuario) {
+		SimpleMailMessage email = new SimpleMailMessage();
+		
+		email.setSubject("Compra Finalizada com Sucesso");
+		email.setTo(usuario.getEmail());
+		email.setText("Compra aprovada com sucesso com o valor de "+carrinho.getTotal());
+		email.setFrom("nao.responda@gmail.com");
+		
+		sender.send(email);
 	}
 }
